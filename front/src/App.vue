49be35589d0eed9fe7842a07ea599c9705ee2b26
bilal-webdeveloper <82528588+bilal-webdeveloper@users.noreply.github.com>
2022-03-09 18:54:37 +0100

    sign in page

---
+++ front/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import LoginVue from "./components/Login.vue"
+
+export default {
+  name: "App",
+  components: {
+    LoginVue
+  }
+}
+</script>
+
+<template>
+  <h1>Groupomania</h1>
+  <router-view></router-view>
+</template>
+
+<style></style>
