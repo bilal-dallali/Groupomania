2326f3cb45c46817ec3ea131d67dbdbac7d26b24
bilal-webdeveloper <82528588+bilal-webdeveloper@users.noreply.github.com>
2022-03-11 23:15:27 +0100

    starting profile page

---
+++ front/src/routes/router.js
@@ -0,0 +1,13 @@
+import WallPage from "../pages/Wall/WallPage.vue"
+import LoginPage from "../pages/LoginPage.vue"
+import ProfilePage from "../pages/ProfilePage.vue"
+import { createRouter, createWebHistory } from "vue-router"
+
+const routes = [
+    { path: "/login", component: LoginPage },
+    { path: "/home", component: WallPage },
+    { path: "/profile", component: ProfilePage }
+]
+const router = createRouter({ history: createWebHistory(), routes })
+
+export { router }
