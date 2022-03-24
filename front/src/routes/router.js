b17178b76b86daea14a8acba20414bf3d0e499ac
bilal-webdeveloper <82528588+bilal-webdeveloper@users.noreply.github.com>
2022-03-24 18:40:50 +0100

    register page

--- front/src/routes/router.js
+++ front/src/routes/router.js
@@ -1,11 +1,13 @@
 import WallPage from "../pages/Wall/WallPage.vue"
 import LoginPage from "../pages/LoginPage.vue"
+import RegisterPage from "../pages/RegisterPage.vue"
 import ProfilePage from "../pages/ProfilePage.vue"
 import EditProfile from "../pages/EditProfile.vue"
 import { createRouter, createWebHistory } from "vue-router"
 
 const routes = [
     { path: "/login", component: LoginPage },
+    { path: "/register", component: RegisterPage },
     { path: "/home", component: WallPage },
     { path: "/profile", component: ProfilePage },
     { path: "/edit-profile", component: EditProfile},
