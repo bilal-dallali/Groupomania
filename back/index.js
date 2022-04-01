60899d8d46e9dce4dd4dfddaf423d2b0d1c9a9f9
bilal-webdeveloper <82528588+bilal-webdeveloper@users.noreply.github.com>
2022-04-01 22:30:30 +0200

    user routes made with password hashing and cookies session

--- back/index.js
+++ back/index.js
@@ -1,18 +1,125 @@
 const express = require("express")
-const bodyParser = require("body-parser")
-const userRoutes = require("./routes/user.routes")
-require("dotenv").config({path: "./config/.env"})
-require("./config/db")
+require("dotenv").config("/.env")
 const app = express()
+const mysql = require("mysql2")
+const cors = require("cors")
+const bodyparser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
+const bcrypt = require("bcrypt")
+//const { response } = require("express")
+const saltRounds = 10
 const port = process.env.PORT
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true
+}))
+app.use(cookieParser())
+app.use(bodyparser.urlencoded({ extended: true }))
 
-// Routes
-app.use("/api/user", userRoutes)
+app.use(session({
+    key: "userId",
+    secret: "subscribe",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 60 * 60 * 24,
+    }
+}))
 
-// Server
-app.listen(port, () => {
-    console.log("server running on port " + port)
+const db = mysql.createConnection({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 })
+
+db.connect((err) =>{
+    if(err) {
+        console.log(err, "MYSQL FAILED TO CONNECT")
+    } else {
+        console.log("MYSQL CONNECTED !!!!")
+    }
+})
+
+
+app.get("/users/login", (req, res) => {
+    db.query("SELECT * FROM Users;", (err, result) => {
+        if (err) {
+            res.status(400).json(err)
+        } else {
+            res.status(200).json(result)
+        }
+    })
+})
+
+app.post("/users/signup", (req, res) => {
+    const { username, email, password } = req.body
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if(err) {
+            console.log(err, "Something went wrong")
+        } else {
+            console
+        }
+        db.query(
+            "INSERT INTO Users (username, email, password) VALUES (?, ?, ?);",
+            [username, email, hash],
+            (err, result) => {
+                if(err) {
+                    res.status(400).json(err)
+                    console.log(err)
+                } else {
+                    res.status(200).json(result)
+                    console.log("req.body", req.body)
+                }
+            }
+        )
+    })
+    
+})
+
+app.get("/users/login", (req, res) => {
+    if (req.session.user) {
+        res.send({ loggedIn: true, user: req.session.user })
+    } else {
+        res.send({ loggedIn: false })
+    }
+})
+
+app.post("/users/login", (req, res) => {
+    const { email, password } = req.body
+
+    db.query(
+        "SELECT * FROM Users WHERE email = ?",
+        email,
+        (err, result) => {
+            if(err) {
+                res.send({ err: err })
+            }
+            if(result.length > 0) {
+                bcrypt.compare(password, result[0].password, (error, response) => {
+                    if (response) {
+                        req.session.user = result
+                        console.log(req.session.user)
+                        res.send(result)
+                    } else {
+                        res.send({ message: "Email or password invalid !"})
+                    }
+                })
+            }
+            else {
+                res.send({ message: "User doesn't exist !" })
+            }
+        }
+    )
+})
+
+app.put("/users", (req, res) => {})
+
+app.delete("/users/:id", (req, res) => {})
+
+app.listen(port, () => console.log("server running on port " + port))
--- back/routes/user.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express")
-const authController = require("../controllers/auth.controller")
-
-router.post("./signup", authController.signUp)
-
-module.exports = router
